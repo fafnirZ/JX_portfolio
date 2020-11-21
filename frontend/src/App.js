e59f9d5f8b0a7b7608c383b04c8557436d083612
Jacky Xie <z5257079@cse.unsw.edu.au>
2020-11-21 21:25:55 +1100

    a

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import Navbar from "./Components/Nav/nav.js"
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      	<Navbar/>
+
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Nav/MenuItems.js
@@ -0,0 +1,14 @@
+export const MenuItems = [
+	{
+		title: 'First',
+		url: '',
+		cName: 'nav-links'
+	},
+
+	{
+		title: 'Second',
+		url: '',
+		cName: 'nav-links'
+	},
+	
+]
