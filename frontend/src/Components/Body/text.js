7ec81f2a3976be8d9bc1741ba8cd47c208255265
Jacky Xie <z5257079@cse.unsw.edu.au>
2020-12-11 21:48:43 +1100

    a

--- frontend/src/Components/Body/text.js
+++ frontend/src/Components/Body/text.js
@@ -2,12 +2,11 @@ export const texts =
 [
 
 	{
-		title: 'lorem impsadlkkjklsfjklfajkl',
-		body: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud 
-		exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate 
-		velit essecillum dolore eu fugiat nulla pariatur. 
-		Excepteur sint occaecat cupidatat noproident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+		title: 'About Me',
+		body: `WHOAMI? I am a budding third year student undertaking a bachelors in Software Engineering at 
+		the University of New South Wales. I strive to achieve a vastly diversified
+		portfolio of projects in multiple fields as I believe in this rapidly changing era of technology; 
+		being a 'Jack of All Trades' would be a great addition to any team when trying to engineer a solution`,
 		title_style: "about_title",
 		body_style: "about_style",
 		image:"macsublime.jpg"
