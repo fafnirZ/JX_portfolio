1f20041d2f5da38dd3635641644d7038bf1d7454
Jacky Xie <z5257079@cse.unsw.edu.au>
2020-12-11 23:39:34 +1100

    a

--- frontend/src/Components/Nav/nav.js
+++ frontend/src/Components/Nav/nav.js
@@ -1,64 +1,83 @@
-import React, { Component } from 'react'
+import React, {useEffect} from 'react'
 import { MenuItems, MenuIcons } from './MenuItems'
 import {Button} from '../button'
-import {Link, animateScroll as scroll} from "react-scroll"
+import {Link, animateScroll as scroll, scrollSpy} from "react-scroll"
 import './nav.css'
 
 
-class Navbar extends Component {
-	state = { clicked: false };
+function Navbar () {
+	const [clicked, setClicked] = React.useState(false);
+	const [scrolled, setScrolled] = React.useState(false);
+
+
+
+	const handleClick = () => {
+		setClicked(!clicked);
+	};
+
+	const handleScroll = () => {
+		const offset = window.scrollY;
+		if(offset > 200) {
+			setScrolled(true);
+		}
+		else {
+			setScrolled(false);
+		}
+	}
+
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll)
+	})
+
+
+	return (
+		<nav id='navbar' className= "NavbarItems">
+			<h1 className="navbar-logo">Jacky Xie<i className="fab fa-react"></i></h1>
+			<div className= "menu-icon" onClick={handleClick}>
+				<i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+
+			</div>
+
+			<ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+		
+				{MenuIcons.map((item, index) => {
+					return (
+						<li className={item.cName}>
+							<a className={item.Icon} href={item.url}/>
+						</li>
+						)
+
+					})
+
+				}
+
+				{MenuItems.map((item, index) => {
+					return (
+						<li>
+							<a className={item.cName}>
+								<Link 
+									to={item.url}									    spy={true}
+								    smooth={true}
+								    offset={-70}
+								    duration={500}
+								>
+								{item.title}
+
+								</Link>
+							</a>
+						</li>
+						)
+
+					})
+				} 
+
+			</ul>
+
+		</nav>
+
+	)
 	
-	handleClick = () => {
-		this.setState({clicked : !this.state.clicked})
-	}
-
-	render() {
-		return (
-			<nav className= "NavbarItems">
-				<h1 className="navbar-logo">Jacky Xie<i className="fab fa-react"></i></h1>
-				<div className= "menu-icon" onClick={this.handleClick}>
-					<i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-
-				</div>
-
-				<ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-			
-					{MenuIcons.map((item, index) => {
-						return (
-							<li className={item.cName}>
-								<a className={item.Icon} href={item.url}/>
-							</li>
-							)
-
-						})
-
-					}
-
-					{MenuItems.map((item, index) => {
-						return (
-							<li>
-								<a className={item.cName}>
-									<Link 
-										to={item.url}									    spy={true}
-									    smooth={true}
-									    offset={-70}
-									    duration={500}
-									>
-									{item.title}
-									</Link>
-								</a>
-							</li>
-							)
-
-						})
-					} 
-
-				</ul>
-
-			</nav>
-
-		)
-	}
 }
 
 export default Navbar
